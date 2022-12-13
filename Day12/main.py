4f70bf799e9d4906ec07bae246a29b361929802d
Kopylov Stanislav <kopylowst@yandex.ru>
2022-12-14 00:43:44 +0300

    Optimized Day 12

--- Day12/main.py
+++ Day12/main.py
@@ -1,6 +1,3 @@
-import copy
-from functools import reduce
-
 lines = open("input.txt").read().split('\n')
 
 for i in range(len(lines)):
@@ -11,29 +8,20 @@ for i in range(len(lines)):
         finishNode = (i, lines[i].index('S'))
         lines[i] = lines[i].replace('S', 'a')
 
-matrix = [[0 for j in range(len(lines[i]))] for i in range(len(lines))]
-toCheckNode = [(startNode[0], startNode[1], 0)]
-
-def validateAndAdd(sourceNode, targetNode):
-    if targetNode[0] < 0 or targetNode[0] >= len(matrix):
-        return
-    if targetNode[1] < 0 or targetNode[1] >= len(matrix[0]):
-        return
-    if matrix[targetNode[0]][targetNode[1]] <= targetNode[2] and matrix[targetNode[0]][targetNode[1]] != 0:
-        return
-    if ord(lines[sourceNode[0]][sourceNode[1]]) - 1 <= ord(lines[targetNode[0]][targetNode[1]]):
-        matrix[targetNode[0]][targetNode[1]] = targetNode[2]
-        toCheckNode.append(targetNode)
+matrix = [[0 for _ in range(len(lines[i]))] for i in range(len(lines))]
+toCheckNode = [startNode]
 
 while toCheckNode:
-    index = min(range(len(toCheckNode)), key=lambda i: toCheckNode[i][2])
-    sourceNode = toCheckNode[index]
-    toCheckNode = toCheckNode[:index] + toCheckNode[index + 1:]
-
-    validateAndAdd(sourceNode, (sourceNode[0] + 1, sourceNode[1], sourceNode[2] + 1))
-    validateAndAdd(sourceNode, (sourceNode[0] - 1, sourceNode[1], sourceNode[2] + 1))
-    validateAndAdd(sourceNode, (sourceNode[0], sourceNode[1] + 1, sourceNode[2] + 1))
-    validateAndAdd(sourceNode, (sourceNode[0], sourceNode[1] - 1, sourceNode[2] + 1))
+    sourceNode = toCheckNode.pop(0)
+    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        targetNode = (sourceNode[0] + dx, sourceNode[1] + dy)
+        if targetNode[0] not in range(len(matrix)) or targetNode[1] not in range(len(matrix[0])):
+            continue
+        if matrix[targetNode[0]][targetNode[1]] != 0:
+            continue
+        if ord(lines[sourceNode[0]][sourceNode[1]]) - 1 <= ord(lines[targetNode[0]][targetNode[1]]):
+            matrix[targetNode[0]][targetNode[1]] = matrix[sourceNode[0]][sourceNode[1]] + 1
+            toCheckNode.append(targetNode)
 
 print(matrix[finishNode[0]][finishNode[1]])
 print(min([matrix[i][j] for j in range(len(matrix[i])) for i in range(len(matrix)) if lines[i][j] == 'a' and matrix[i][j] != 0]))
