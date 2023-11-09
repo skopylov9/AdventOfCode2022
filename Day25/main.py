ec141518143e89aa7b37deaaf87379342377842c
Kopylov Stanislav <kopylowst@yandex.ru>
2023-11-09 18:57:16 +0300

    Solved Day 25

---
+++ Day25/main.py
@@ -0,0 +1,15 @@
+def decimalToSnafu(decimal):
+    value = ''
+    while decimal:
+        value += str(decimal % 5)
+        decimal = (decimal // 5) + (1 if decimal % 5 > 2 else 0)
+    return value.replace('3', '=').replace('4', '-')[::-1]
+
+def snafuToDecimal(snafuStr):
+    value = 0
+    for idx, ch in enumerate(snafuStr[::-1]):
+        value += pow(5, idx) * int(ch.replace('-', '-1').replace('=', '-2'))
+    return value
+
+# Part 1: 122-12==0-01=00-0=02
+print(decimalToSnafu(sum([snafuToDecimal(snafuNumber) for snafuNumber in open('input.txt').read().split('\n')])))
